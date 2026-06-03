f64cff9f40e4e0899624958cfc2f9a8a706a644c
Keyla Franco Hidalgo <67887869+KeylaFrancoH@users.noreply.github.com>
2026-06-02 21:27:51 -0500

    species

--- middleware.js
+++ middleware.js
@@ -5,7 +5,6 @@ export default function middleware(req) {
     headers: {
       'Referrer-Policy': 'origin-when-cross-origin',
       'X-Frame-Options': 'DENY',
-      'X-Content-Type-Options': 'nosniff',
       'X-DNS-Prefetch-Control': 'on',
       'Strict-Transport-Security':
         'max-age=31536000; includeSubDomains; preload',
